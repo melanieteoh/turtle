b09e9444a3ca37ac0685d5fc463eddf0f2fb33f1
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-28 00:25:35 +0800

    Create spyglass.py

---
+++ spyglass.py
@@ -0,0 +1,23 @@
+from turtle import*
+exitonclick()
+
+width(5)
+forward(23)
+
+width(13)
+forward(20)
+
+width(15)
+forward(20)
+
+width(20)
+forward(25)
+
+color("transparent")
+left(180)
+width(1)
+forward(2)
+
+color("white")
+width(13)
+forward(1)
