d3c12120f9c7ce2206011b27c5a70754281f70f8
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-30 22:23:40 +0800

    Create steps.py

---
+++ steps.py
@@ -0,0 +1,19 @@
+from turtle import *
+exitonclick()
+
+step_height = input("Enter step height.")
+step_width = input("Enter step width.")
+
+forward(step_width)
+left(90)
+forward(step_height)
+right(90)
+forward(step_width)
+left(90)
+forward(step_height)
+right(90)
+forward(step_width)
+left(90)
+forward(step_height)
+right(90)
+forward(step_width)
