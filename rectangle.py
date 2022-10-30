e57666ab0a4733ab22f489036aa3bb054ebb5b58
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-30 22:22:53 +0800

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+from turtle import *
+exitonclick()
+
+length = input("What's the length of the rectangle?")
+width = input("What's the width of the rectangle?")
+
+forward(length)
+left(90)
+forward(width)
+left(90)
+forward(length)
+left(90)
+forward(width)
