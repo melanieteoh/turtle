893b087a6bf3523fe53aaeb2fb0ca3071c1fbb3c
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-30 22:27:39 +0800

    Create city.py

---
+++ city/city.py
@@ -0,0 +1,48 @@
+from turtle import *
+
+speed(500)
+from landscape import*
+exitonclick()
+
+
+width(1)
+penup()
+right(180)
+forward(870)
+left(90)
+forward(265)
+pendown()
+from stars import*
+
+left(160)
+penup()
+forward(150)
+right(270)
+
+pendown()
+color("white")
+from houses import*
+
+penup()
+right(120)
+forward(600)
+left(175)
+pendown()
+from building2 import*
+
+penup()
+right(90)
+forward(330)
+left(90)
+forward(100)
+left(90)
+pendown()
+from klcc1 import*
+
+penup()
+right(135)
+forward(100)
+left(90)
+forward(200)
+pendown()
+from houses import* 
