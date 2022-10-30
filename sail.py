731520ec09b3752260afb974a136f119794d0906
melanieteoh <92512366+melanieteoh@users.noreply.github.com>
2022-10-30 22:21:36 +0800

    Create sail.py

---
+++ sail.py
@@ -0,0 +1,27 @@
+from turtle import*
+
+c = input("What colour of the sail do you want?")
+l = input("What colour of the boat do you want?")
+
+color(c)
+width(4)
+forward(50)
+left(135)
+forward(70)
+left(135)
+forward(50)
+
+color(l)
+width(3)
+right(90)
+forward(50)
+left(135)
+forward(50)
+left(45)
+forward(80)
+left(45)
+forward(50)
+left(135)
+forward(110)
+
+exitonclick()
